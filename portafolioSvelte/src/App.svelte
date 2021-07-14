89786ecd7323d0b23d62e63f31f63f209fbf4f47
Ariel Macario <arielmacario.11@gmail.com>
2021-07-13 21:24:28 -0600

    Portafolio Nav y descripcion

---
+++ portafolioSvelte/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	
+</script>
+
+<main>
+	<br>
+	<nav class="navbar navbar-expand-lg navbar-light">
+		<a class="navbar-brand" href="#">
+			<img src="images/logoMacario.png" alt="" height="40">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link text-warning lead mr-auto" href="#">Tutoria</a>
+					</li>
+				</ul>
+
+			<form class="form-inline my-2 my-lg-0">
+				<button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Contactame</button>
+			  </form>
+			  
+		</div>
+	</nav>
+	<br>
+	<br>
+	<h1 class="display-5 text-center p-5">Front-end Developer and Back-end Developer</h1>
+	
+	<h4 class="text-center lead">I design and code beautifully simple things, and I love what I do.</h4>
+
+	<img src="images/imageAbstract.png" alt="Dia a Dia" class="rounded mx-auto d-block">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</main>
+
+<style>
+	@import url("https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css");
+	@media (min-width: 992px) {
+  .navbar-expand-lg {
+    padding-right: 10rem;
+    padding-left: 10rem;
+  }
+}
+</style>
