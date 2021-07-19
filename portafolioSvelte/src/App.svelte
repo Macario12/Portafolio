ea085ab5b265b187eef932b0c19b51ce0372c66b
Ariel Macario <arielmacario.11@gmail.com>
2021-07-19 11:27:16 -0600

    update build

--- portafolioSvelte/src/App.svelte
+++ portafolioSvelte/src/App.svelte
@@ -1,79 +1,27 @@
 <script>
 	import Contact from './components/Contact.svelte';
-
-	import Footer from './components/Footer.svelte'
-	import Navbar from './components/Navbar.svelte'
-	import Projects from './components/Projects.svelte'
+	
+    import { Router, Route} from 'svelte-navigator';
+	import Navbar from './components/Navbar.svelte';
+	import Home from './components/Home.svelte';
 </script>
-
-<main>
+<Router>
+<header>
 	<br>
 	<Navbar/>
-	<br>
-	<br>
-	<h1 class="display-5 text-center p-5">Desarrollador Front-end y Desarrollador Brack-end.</h1>
+</header>
+<main>
+	<Route path="contact">
+      <Contact />
+    </Route>
 	
-	<h4 class="text-center lead">Desarrollo aplicaciones, amo lo que hago, me encantan los animales.</h4>
-	<img src="./images/avtarMacario.png" alt="AvatarMac" class="rounded mx-auto d-block img-fluid" width="250px"height= "210px">
-	<br>
-	<br>
-	<img src="./images/imageAbstract.png" alt="Dia a Dia" class="rounded mx-auto d-block img-fluid">
-	<div style="position: relative;">
-		<div class="p-3 mb-2 bg-warning" >
-			<br>
-			<br>
-			<h3 class="display-5 text-center p-5 text-white font-weight-bold">Hola, Soy Ariel, Un gusto.</h3>
-			<div class="text-wrap mx-auto" >
-				<p class="lead text-white text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatibus voluptates voluptate quos corporis odit temporibus sapiente fugiat cum ut. Laudantium cupiditate dignissimos optio esse animi incidunt corporis tenetur ullam.</p>
-			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-		<div class="container bg-white mx-auto">
-			<div class="row">
-			  <div class="col-sm">
-				  <br>
-				<img src="./images/frontend.png" alt="front-end" class="rounded mx-auto d-block img-fluid">
-				<h3 class="display-5 text-center p-5 font-weight-bold">Front-end.</h3>
-				<p class="text-center lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, facilis dignissimos natus recusandae amet enim delectus sequi assumenda, libero nihil molestiae magnam! Obcaecati sed necessitatibus cum eum consequuntur tempore blanditiis.</p>
-				<h6 class="text-center text-warning">Things I enjoy designing:</h6>
-				<p class="text-center lead">Html, CSS</p>
-				<h6 class="text-center text-warning">Dev Tools:</h6>
-				<p class="text-center lead">Angular</p>
-				<p class="text-center lead">React Js</p>
-				<p class="text-center lead">Svelte Js</p>
-			  </div>
-			  <div class="col-sm">
-				<br>
-				<img src="./images/backend.png" alt="back-end" class="rounded mx-auto d-block img-fluid">
-				<h3 class="display-5 text-center p-5 font-weight-bold">Back-end.</h3>
-				<p class="text-center lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium possimus quaerat! Laborum impedit dignissimos quod quaerat corrupti quia autem provident, quos assumenda harum possimus animi ratione beatae fugiat unde?</p>
-				<h6 class="text-center text-warning">Languages I speak:</h6>
-				<p class="text-center lead">Html, CSS</p>
-				<h6 class="text-center text-warning">Dev Tools:</h6>
-				<p class="text-center lead">Angular</p>
-				<p class="text-center lead">React Js</p>
-				<p class="text-center lead">Svelte Js</p>
-			  </div>
-			</div>
-		  </div>
-	</div>
-	
-	<Projects/>
-
-	<Footer/>
+	<Route path="/">
+		<Home/>
+	</Route>
 	
 </main>
 
+</Router>
 <style>
 	@import url("https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css");
-	@media (min-width: 992px) {
-  .navbar-expand-lg {
-    padding-right: 10rem;
-    padding-left: 10rem;
-  }
-
-}
-
 </style>
--- portafolioSvelte/src/components/Contact.svelte
+++ portafolioSvelte/src/components/Contact.svelte
@@ -1,8 +1,57 @@
 <script>
-import Navbar from "./Navbar.svelte";
+    /* const sendmail = require('sendmail')(); */
+    /* import sendmail from 'sendmail'; */
+    let mensaje = {
+        nombre: "",
+        email: "",
+        mensaje: ""
+    }
+
+    const sumbitMenssage = e =>{
+        e.preventDefault();
+        /* sendmail({
+        from: mensaje.email,
+        to: 'arielmacario.11@gmail.com',
+        subject: mensaje.nombre,
+        html: mensaje.mensaje,
+        }, function(err, reply) {
+            console.log(err && err.stack);
+            console.dir(reply);
+        }); */
+        console.log(mensaje)
+    };
+
+    
 </script>
-    <Navbar/>
-    <h1>Contact</h1>
+   <div class="p-5">
+       <br>
+        <h1 class="text-center">Contactame</h1>
+
+        <br>
+
+        <form class="p-5" on:submit={sumbitMenssage}>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputEmail4">Nombre</label>
+                  <input bind:value={mensaje.nombre} type="text" class="form-control" id="inputEmail4" placeholder="nombre" >
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="inputPassword4">Correo electronico</label>
+                  <input bind:value={mensaje.email} type="email" class="form-control" id="inputPassword4" placeholder="correo eletronico" >
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputAddress">Mensaje</label>
+                
+                <textarea  bind:value={mensaje.mensaje} name="message" class="form-control" id="" cols="30" rows="10"></textarea>
+              </div>
+              
+              <div class="text-center">
+                  <br>
+                <button type="submit" class="btn btn-outline-warning btn-lg">Enviar</button>
+              </div>
+          </form>
+   </div>
 <style>
     @import url("https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css");
 </style>
